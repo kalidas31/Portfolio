0bde3d48479ed84d620fb9766a21dbbc4872173d
kalidas ch <118943313+kalidas31@users.noreply.github.com>
2024-12-30 20:38:22 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,24 +8,7 @@ import "animate.css";
 import TrackVisibility from "react-on-screen";
 
 export const Projects = () => {
-  // const projects = [
-  //   {
-  //     title: "Business Startup",
-  //     description: "Design & Development",
-  //     imgUrl: projImg1,
-  //   },
-  //   {
-  //     title: "Business Startup",
-  //     description: "Design & Development",
-  //     imgUrl: projImg2,
-  //   },
-  //   {
-  //     title: "Business Startup",
-  //     description: "Design & Development",
-  //     imgUrl: projImg3,
-  //   },
-  // ];
-
+  
   return (
     <section className="project" id="projects">
       <Container>
